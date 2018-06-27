d08d54b81465c45119eff02a45447a7af0b30b04
Ming Xiao <mxiao@pivotal.io>
2018-06-27 15:02:59 -0700

    Add react navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 const CLOUD_CONTROLLER_URL = "https://api.sys.oakley.cf-app.com";
 const accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleS0xIiwidHlwIjoiSldUIn0.eyJqdGkiOiJmNWFkNDE3Nzg0MzI0NTVjOWZkNjg2ZTExOGQwNzM4ZCIsInN1YiI6IjE4YWFiZjY3LTgyYTktNGM4MC1hYzgzLTQxODQ1ZGRlMDE5NCIsInNjb3BlIjpbImNsb3VkX2NvbnRyb2xsZXIucmVhZCIsImFjdHVhdG9yLnJlYWQiLCJjbG91ZF9jb250cm9sbGVyLndyaXRlIiwiY2xvdWRfY29udHJvbGxlci51c2VyIiwib3BlbmlkIiwiY2xvdWRfY29udHJvbGxlci5hZG1pbiJdLCJjbGllbnRfaWQiOiJhcHBzX21hbmFnZXJfanMiLCJjaWQiOiJhcHBzX21hbmFnZXJfanMiLCJhenAiOiJhcHBzX21hbmFnZXJfanMiLCJ1c2VyX2lkIjoiMThhYWJmNjctODJhOS00YzgwLWFjODMtNDE4NDVkZGUwMTk0Iiwib3JpZ2luIjoidWFhIiwidXNlcl9uYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluIiwiYXV0aF90aW1lIjoxNTMwMTE4MTIwLCJyZXZfc2lnIjoiYzgzYTM3N2MiLCJpYXQiOjE1MzAxMTk5MzQsImV4cCI6MTUzMTMyOTUzNCwiaXNzIjoiaHR0cHM6Ly91YWEuc3lzLm9ha2xleS5jZi1hcHAuY29tL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbImNsb3VkX2NvbnRyb2xsZXIiLCJhY3R1YXRvciIsIm9wZW5pZCIsImFwcHNfbWFuYWdlcl9qcyJdfQ.WM59v7ikPhLSOBiXOgRASLjMqOWeX8E3ns_pMpV2QhxplqtYGfcqNqYsp2oqsnj14XsQ3Wj8LjBRG74T81rh15CDlyUR8ju53gs5LczsU9qCAhal-j730CZqerIXICPp02j-XWlY1qhY32Y91QTCMArdIB2vrUjt-kGGuBhCCqmeEsQBQ3wbLab6RHGBOpWcPrWg6dqitFfXcGtgiTgAwWGcS5CGGc1LOa1QslYnPjxXyW52V24SRbCx85asWY2jGoicaVt0E_mIPtEYVAvRbXKRaTpESVNz70YF0JjEsI7LYfJdc13GCTnbuJ5hzlgAERjJaE0TRFwjEsX9bAeqKQ"
@@ -7,7 +7,7 @@ const accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleS0xIiwidHlwIjoiSldUIn0.eyJ
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {page: 'orgs'};
   }
 
   componentDidMount = async () => {
@@ -30,9 +30,14 @@ export default class App extends React.Component {
 
   render() {
     const {orgs = []} = this.state;
+    const content = orgs.map(org => {
+      return <View style={styles.orgs}><Button key={org.metadata.guid}
+                                               title={org.entity.name}>{org.entity.name}</Button></View>
+    });
     return (
       <View style={styles.container}>
-        <Text>You have {orgs.length} orgs</Text>
+        <View style={styles.header}><Text>You have {orgs.length} orgs</Text></View>
+        {content}
       </View>
     );
   }
@@ -46,4 +51,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0543a8'
+  },
+  orgs: {
+    flex: 3,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#bdffaf',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  }
 });
